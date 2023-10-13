3dfc4d4b4307a57192f3750af2a4cdbb13f50663
gwave <adebayooluwatobi2018@gmail.com>
2023-10-13 13:52:09 +0100

    i

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const app = express()
 //set port
 const PORT = process.env.PORT || 3000;
 
-let corsOptions = {
-    origin: 'http://localhost:3000'
-}
+const corsOptions = {
+    origin: function (origin, callback) {
+        const allowedOrigins = [
+            'http://localhost:3000',
+            'https://my-app-tan-kappa.vercel.app' 
+        ];
+
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
 
 const db = require('./models')
 db.sequelize.sync({})
